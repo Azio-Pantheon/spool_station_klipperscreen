7adc0ca18cf33d6b3dbf75a1b1ffececf3b5216b
azio <azio@pantheondesign.ca>
2024-04-23 17:50:30 -0700

    feature/hide alibrate PID function

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -278,7 +278,6 @@ class Panel(MenuPanel):
             self.labels["keypad"] = Keypad(self._screen, self.change_target_temp, self.pid_calibrate, self.hide_numpad)
         can_pid = self._printer.state not in ("printing", "paused") \
             and self._screen.printer.config[self.active_heater]['control'] == 'pid'
-        self.labels["keypad"].show_pid(can_pid)
         self.labels["keypad"].clear()
 
         if self._screen.vertical_mode:
