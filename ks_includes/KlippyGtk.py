2355868f1f875497b0aa2b588012881b6cc55787
azio <azio@pantheondesign.ca>
2024-04-24 18:46:07 -0700

    scaled down all font size a tiny bit

--- ks_includes/KlippyGtk.py
+++ ks_includes/KlippyGtk.py
@@ -39,7 +39,7 @@ class KlippyGtk:
         self.width = screen.width
         self.height = screen.height
         self.font_ratio = [33, 49] if self.screen.vertical_mode else [43, 29]
-        self.font_size = min(self.width / self.font_ratio[0], self.height / self.font_ratio[1])
+        self.font_size = min(self.width / self.font_ratio[0], self.height / self.font_ratio[1]) * 0.96
         self.img_scale = self.font_size * 2
         self.button_image_scale = 1.38
         self.bsidescale = .65  # Buttons with image at the side
