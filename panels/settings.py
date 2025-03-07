69300ab4003c8f58bd4d42baa132968d57dca622
Azio Tu <azio@pantheondesign.com>
2025-03-07 15:18:38 -0800

    removed language sn setttings panel

--- panels/settings.py
+++ panels/settings.py
@@ -11,11 +11,7 @@ class Panel(ScreenPanel):
         self.printers = self.settings = self.langs = {}
         self.menu = ['settings_menu']
         options = self._config.get_configurable_options().copy()
-        options.append({"lang": {
-            "name": _("Language"),
-            "type": "menu",
-            "menu": "lang"
-        }})
+
 
         self.labels['settings_menu'] = self._gtk.ScrolledWindow()
         self.labels['settings'] = Gtk.Grid()
@@ -23,16 +19,6 @@ class Panel(ScreenPanel):
         for option in options:
             name = list(option)[0]
             self.add_option('settings', self.settings, name, option[name])
-
-        self.labels['lang_menu'] = self._gtk.ScrolledWindow()
-        self.labels['lang'] = Gtk.Grid()
-        self.labels['lang_menu'].add(self.labels['lang'])
-        for lang in self._config.lang_list:
-            self.langs[lang] = {
-                "name": lang,
-                "type": "lang",
-            }
-            self.add_option("lang", self.langs, lang, self.langs[lang])
 
         self.labels['printers_menu'] = self._gtk.ScrolledWindow()
         self.labels['printers'] = Gtk.Grid()
@@ -111,12 +97,7 @@ class Panel(ScreenPanel):
             open_menu.set_hexpand(False)
             open_menu.set_halign(Gtk.Align.END)
             dev.add(open_menu)
-        elif option['type'] == "lang":
-            select = self._gtk.Button("load", style="color3")
-            select.connect("clicked", self._screen.change_language, option['name'])
-            select.set_hexpand(False)
-            select.set_halign(Gtk.Align.END)
-            dev.add(select)
+
 
         opt_array[opt_name] = {
             "name": option['name'],
